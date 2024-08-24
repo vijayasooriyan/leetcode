83b55946c996ee479f386cafb36028be058b07d3
Vijayasooriyan Kamarajah <155728077+vijayasooriyan@users.noreply.github.com>
2024-08-24 16:38:50 +0530

    Time: 667 ms (31.05%), Space: 0B (100.00%) - LeetHub

--- 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -2,7 +2,7 @@
 select round(avg(order_date = customer_pref_delivery_date)*100, 2) as immediate_percentage
 from Delivery
 where (customer_id, order_date) in (
-  Select customer_id, min(order_date) 
+  select customer_id, min(order_date) 
   from Delivery
   group by customer_id
 );
